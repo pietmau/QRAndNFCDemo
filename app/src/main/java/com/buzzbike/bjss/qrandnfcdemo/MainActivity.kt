90a77bf96f027f225798c921e8a66d48c40051b1
Maurizio Pietrantuono <maurizio.pietrantuono@bjss.com>
2018-02-26 11:23:36 +0000

    First commit

---
+++ app/src/main/java/com/buzzbike/bjss/qrandnfcdemo/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.buzzbike.bjss.qrandnfcdemo
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+  private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+    when (item.itemId) {
+      R.id.qrcode -> {
+        return@OnNavigationItemSelectedListener true
+      }
+      R.id.nfc -> {
+        return@OnNavigationItemSelectedListener true
+      }
+    }
+    false
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+  }
+}
